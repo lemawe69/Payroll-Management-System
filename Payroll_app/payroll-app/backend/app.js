b381fc03bf1b4bad851dcb39c57abd1f592b0c99
lemawe69 <brandonlemmer777@gmail.com>
2025-09-10 11:00:56 +0200

    Update 2

--- Payroll_app/payroll-app/backend/app.js
+++ Payroll_app/payroll-app/backend/app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
-app.use(express.json()); // to parse JSON request bodies
+app.use(express.json());
 
 const payrollRoutes = require('./routes/payrollRoutes');
 app.use('/api/payroll', payrollRoutes);
